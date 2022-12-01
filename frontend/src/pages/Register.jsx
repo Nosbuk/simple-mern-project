0a10e842c6eb18c93047757588c98e7049250a0c
j.matwis <j.matwis@cheil.com>
2022-12-01 19:17:23 +0100

    feat: auth components setup

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,76 @@
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
 export const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = ({ target }) => {
+    setFormData((prev) => ({
+      ...prev,
+      [target.name]: target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  const { name, email, password, confirmPassword } = formData;
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className="form">
+        <form className="form-group" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="from-control"
+            id="name"
+            name="name"
+            value={name}
+            placeholder="Enter your name"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            className="from-control"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            className="from-control"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            className="from-control"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={confirmPassword}
+            placeholder="Confirm your password"
+            onChange={handleChange}
+          />
+          <button type="submit" className="btn btn-block">
+            Submit
+          </button>
+        </form>
+      </section>
+    </>
+  );
 };
