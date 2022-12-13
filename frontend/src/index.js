82e2042ec39a035288a7ab0f92bcac7ebfe180a3
j.matwis <j.matwis@cheil.com>
2022-12-13 20:32:20 +0100

    feat: toast setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
 import "./index.css";
-
+import { App } from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
-const root = createRoot(container);
-
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
