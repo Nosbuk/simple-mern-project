6353562ef2a7b70ba6d203a11ef663de8a40f43f
j.matwis <j.matwis@cheil.com>
2022-12-13 20:50:54 +0100

    feat: Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,57 @@
 import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import { Spinner } from "../components/Spinner";
+
 export const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
-  const handleChange = ({ target }) => {
-    setFormData((prev) => ({
-      ...prev,
-      [target.name]: target.value,
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
     }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
-  const { name, email, password, confirmPassword } = formData;
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -26,29 +61,37 @@ export const Login = () => {
         </h1>
         <p>Login and start setting goals</p>
       </section>
+
       <section className="form">
-        <form className="form-group" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            className="from-control"
-            id="email"
-            name="email"
-            value={email}
-            placeholder="Enter your email"
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            className="from-control"
-            id="password"
-            name="password"
-            value={password}
-            placeholder="Enter your password"
-            onChange={handleChange}
-          />
-          <button type="submit" className="btn btn-block">
-            Login
-          </button>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
         </form>
       </section>
     </>
