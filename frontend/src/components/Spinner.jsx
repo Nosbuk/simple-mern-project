2ab1ddbb33434fcb374e9a3ac9644ba0fa3a9996
j.matwis <j.matwis@cheil.com>
2022-12-13 20:25:23 +0100

    feat: Spinner component

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Spinner = () => {
+  return (
+    <div className="loadingSpinnerConatiner">
+      <div className="loadingSpinner"></div>
+    </div>
+  );
+};
